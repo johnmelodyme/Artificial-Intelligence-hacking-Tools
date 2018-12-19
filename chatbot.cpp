8f7475df16e60ac3173e50b004920531e2774c4e
JohnMelodyMel <44691957+johnmelodyme@users.noreply.github.com>
2018-12-19 11:51:22 +0800

    Create chatbot.cpp

---
+++ chatbot.cpp
@@ -0,0 +1,38 @@
+/*This is Just a console chatbox*/
+?*Author John Melody*/
+#include<iostream>
+using namespace std;
+ main()
+{
+    char a[50], b[50];
+    char ch[50],n,x[40];
+    char c;
+    cout<<"\nPlease Input name: \n";
+    cin>>ch;
+    cout<<"\nHow do I address you Sir?\n";
+    cin>>a;
+
+        {
+            cout<<"\nHello "<<a<<"\n";
+            cout<<"\nCommand? \n";
+            cin>>ch;
+            }
+
+{
+    cout<<"\nAre you my creator?\n";
+    cin>>ch;
+    cout<<"\n Okay.\n";
+}
+{
+    cout<<"\nYou do know that I am just a prototype right?\n";
+    cin>>ch;
+    cout<<"\n Exactly. \n";
+}
+
+    {
+        cout<<"\n Can I go now? \n";
+        cin>>ch;
+    }
+    cout<<"\n Bot Shutting Down... \n";
+system("Pause");
+}
