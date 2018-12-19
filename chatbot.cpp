047724656fe3e0783a60f6f0887719f42e2a28fb
JohnMelodyMel <44691957+johnmelodyme@users.noreply.github.com>
2018-12-19 14:20:31 +0800

    Update chatbot.cpp

--- chatbot.cpp
+++ chatbot.cpp
@@ -1,5 +1,3 @@
-/*This is Just a console chatbox*/
-/*Author John Melody*/
 #include<iostream>
 using namespace std;
  main()
@@ -7,27 +5,30 @@ using namespace std;
     char a[50], b[50];
     char ch[50],n,x[40];
     char c;
-    cout<<"\nPlease Input name: \n";
-    cin>>ch;
-    cout<<"\nHow do I address you Sir?\n";
+    cout<<"\n Please Input Your Name:  \n";
     cin>>a;
 
         {
-            cout<<"\nHello "<<a<<"\n";
-            cout<<"\nCommand? \n";
+            cout<<"\nHello, "<<a<<"\n";
+            cout<<"\nPlease execute a command: \n";
             cin>>ch;
             }
+                {
+                    cout<<"\n Tell me about yourself. \n";
+                    cin>>ch;
+                    cout<<"\n Interesting... \n";
+                }
 
 {
     cout<<"\nAre you my creator?\n";
     cin>>ch;
     cout<<"\n Okay.\n";
 }
-{
-    cout<<"\nYou do know that I am just a prototype right?\n";
-    cin>>ch;
-    cout<<"\n Exactly. \n";
-}
+    {
+        cout<<"\nYou do know that I am just a prototype right?\n";
+        cin>>ch;
+        cout<<"\n Exactly. \n";
+    }
 
     {
         cout<<"\n Can I go now? \n";
