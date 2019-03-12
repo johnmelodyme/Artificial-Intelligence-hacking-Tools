9370c8047061a97320f9553c4471f114eb4e186d
JohnMelodyMe <Johnmelodyme@gmail.com>
2019-03-12 22:19:39 +0800

    Create Hackingtools.sh

---
+++ Hackingtools.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env BASH
+#By John Melody Me
+apt install figlet
+apt install toilet
+figlet Kali Linux Hacking Tools
+echo KickerNut
+git clone https://github.com/k4m4/kickthemout.git
+echo WifiJammer
+git clone https://github.com/DanMcInerney/wifijammer.git
+echo InSpy
+git clone https://github.com/leapsecurity/InSpy.git
+echo 
+git clone https://github.com/LionSec/katoolin.git
+chmod +x katoolin
+katoolin
